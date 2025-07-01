461bd19ccf84948ef1acc523bd85518f9b07f044
jelg <perpetualmoti0n@outlook.com>
2025-07-01 14:32:43 -0600

    Wasm Hello added

---
+++ Wasm/hello_world/target/wasm32-unknown-unknown/release/deps/hello_world-047f0c5dbee3f7eb.d
@@ -0,0 +1,7 @@
+/home/jelg/Programming-Projects/Rust/Wasm/hello_world/target/wasm32-unknown-unknown/release/deps/libhello_world-047f0c5dbee3f7eb.rmeta: src/lib.rs
+
+/home/jelg/Programming-Projects/Rust/Wasm/hello_world/target/wasm32-unknown-unknown/release/deps/libhello_world-047f0c5dbee3f7eb.rlib: src/lib.rs
+
+/home/jelg/Programming-Projects/Rust/Wasm/hello_world/target/wasm32-unknown-unknown/release/deps/hello_world-047f0c5dbee3f7eb.d: src/lib.rs
+
+src/lib.rs:
---
+++ Wasm/hello_world/target/wasm32-unknown-unknown/release/libhello_world.d
@@ -0,0 +1,1 @@
+/home/jelg/Programming-Projects/Rust/Wasm/hello_world/target/wasm32-unknown-unknown/release/libhello_world.rlib: /home/jelg/Programming-Projects/Rust/Wasm/hello_world/src/lib.rs
